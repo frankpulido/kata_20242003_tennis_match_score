c25a471b4a2a4a3f2bfb5d28aff65b66d0e29168
frankpulido <frankpulido@me.com>
2024-10-20 18:51:38 +0200

    code cleaning and adding display of match and set details

--- play.php
+++ play.php
@@ -7,13 +7,14 @@ class Play {
     protected Player $player1;
     protected Player $player2;
     protected array $setsPlayed;
+    protected array $match;
 
     // Constructor
-    public function __construct(Player $player1, Player $player2, array $setsPlayed = [])
+    //public function __construct(Player $player1, Player $player2, array $setsPlayed = [])
+    public function __construct(Player $player1, Player $player2)
     {
         $this->player1 = $player1;
         $this->player2 = $player2;
-        $this->setsPlayed = $setsPlayed;
     }
 
     // Getter
@@ -26,6 +27,9 @@ class Play {
     public function getSetsPlayed() : array {
         return $this->setsPlayed;
     }
+    public function getMatch() : array {
+        return $this->match;
+    }
 
     // Function GAME : Binary function assigns winner
     public function Game() : string {
@@ -37,51 +41,45 @@ class Play {
     }
 
     // function SET : The Set winner is first to win 6 or 7 games
+
     public function Set() : array {
         $set = 6;
         $game = NULL;
-        //$games = [];
-        $gamesWithKeys = [$this->getPlayer1()->getName() => 0, $this->getPlayer2()->getName() => 0];
-        //$checkTiedFive = [];
+        $gamesWithKeys = [$this->player1->getName() => 0, $this->player2->getName() => 0];
         do {
             $game = $this->Game();
-            if($this->getPlayer1()->getName() == $game) {$gamesWithKeys[$this->getPlayer1()->getName()] += 1;}
-            if($this->getPlayer2()->getName() == $game) {$gamesWithKeys[$this->getPlayer2()->getName()] += 1;}
-            if($gamesWithKeys[$this->getPlayer1()->getName()] == 5 && $gamesWithKeys[$this->getPlayer2()->getName()] == 5) {$set = 7;}
+            if($this->player1->getName() == $game) {$gamesWithKeys[$this->player1->getName()] += 1;}
+            if($this->player2->getName() == $game) {$gamesWithKeys[$this->player2->getName()] += 1;}
+            if($gamesWithKeys[$this->player1->getName()] == 5 && $gamesWithKeys[$this->player2->getName()] == 5) {$set = 7;}
         } while (max($gamesWithKeys) < $set);
+
+        $this->setsPlayed[] = $gamesWithKeys;
         return $gamesWithKeys;
     }
 
     // function MATCH : Match winner is first to win 3 sets. Therefore, a match ranges from 3 to 5 Sets.
     public function Match() {
         $set = NULL;
-        $setsPlayed = [];
-        $matchWithKeys = [$this->getPlayer1()->getName() => 0, $this->getPlayer2()->getName() => 0];
+        $this->match = [$this->player1->getName() => 0, $this->player2->getName() => 0];
         $matchesWonPlayer1 = $this->getPlayer1()->getMatchesWon();
         $matchesWonPlayer2 = $this->getPlayer2()->getMatchesWon();
 
+
         do {
             $set = $this->Set();
-            $setsPlayed[] = $set;
             $winner_name = array_keys($set, max($set));
             //https://stackoverflow.com/questions/7068724/getting-an-array-key-using-the-max-function
-            if($this->getPlayer1()->getName() == $winner_name[0]) {$matchWithKeys[$this->getPlayer1()->getName()] += 1;}
-            if($this->getPlayer2()->getName() == $winner_name[0]) {$matchWithKeys[$this->getPlayer2()->getName()] += 1;}
-            
-            //var_dump($matchWithKeys);
+            if($this->player1->getName() == $winner_name[0]) {$this->match[$this->player1->getName()] += 1;}
+            if($this->player2->getName() == $winner_name[0]) {$this->match[$this->player2->getName()] += 1;}
+        } while (max($this->match) < 3);
 
-        } while (max($matchWithKeys) < 3);
-
-        $this->setsPlayed = $setsPlayed; // No creo un SETTER, el atributo sólo puede recibir el valor ejecutando esta función.
-        if($matchWithKeys[$this->getPlayer1()->getName()] == 3) {$matchesWonPlayer1++; $this->getPlayer1()->setMatchesWon($matchesWonPlayer1); echo "The winner is : " . $this->getPlayer1()->getName() . PHP_EOL;}
-        if($matchWithKeys[$this->getPlayer2()->getName()] == 3) {$matchesWonPlayer2++; $this->getPlayer2()->setMatchesWon($matchesWonPlayer2); echo "The winner is : " . $this->getPlayer2()->getName() . PHP_EOL;}
-
-        return $matchWithKeys;
+        if($this->match[$this->player1->getName()] == 3) {$matchesWonPlayer1++; $this->player1->setMatchesWon($matchesWonPlayer1); echo "The winner is : " . $this->player1->getName() . PHP_EOL;}
+        if($this->match[$this->player2->getName()] == 3) {$matchesWonPlayer2++; $this->player2->setMatchesWon($matchesWonPlayer2); echo "The winner is : " . $this->player2->getName() . PHP_EOL;}
     }
 
     // function Best Set : Set won with greater Games difference, regardless of final match winner
     public function bestSet() : string {
-        $sets = $this->getSetsPlayed();
+        $sets = $this->setsPlayed;
         $index = -1;
         $score1 = 0;
         $score2 = 0;
@@ -95,11 +93,24 @@ class Play {
             if ($maxdiff < abs($score1 - $score2)) {
                 $index = $i;
                 $maxdiff = abs($score1 - $score2);
-                $winner = ($score1 > $score2)? $this->getPlayer1()->getName() : $this->getPlayer2()->getName();
+                $winner = ($score1 > $score2)? $this->player1->getName() : $this->player2->getName();
                 $setScores = $score1 . " - " . $score2;
             }
         }
         return "The greater difference in Games won in a Set was : $maxdiff. It occurred first in Set #" . ($index + 1) . " :" . PHP_EOL . "Winner : $winner" . PHP_EOL . "Scores : $setScores." . PHP_EOL;
+    }
+
+    public function displayMatchScoreDetails() : string {
+        $matchDetails = "";
+        $matchDetails = $this->player1->getName() . " versus " . $this->player2->getName();
+        $matchDetails = $matchDetails . " : [ " . $this->match[$this->player1->getName()] . " - " . $this->match[$this->player2->getName()] . " ]" . PHP_EOL;
+        
+        $setDetails = "[ " . $this->setsPlayed[0][$this->player1->getName()] . " - " . $this->setsPlayed[0][$this->player2->getName()] . " ]";
+        for ($i = 1; $i < count($this->getSetsPlayed()); $i++) {
+            $setDetails = $setDetails . " - " . "[ " . $this->setsPlayed[$i][$this->player1->getName()] . " - " . $this->setsPlayed[$i][$this->player2->getName()] . " ]";
+        }
+        $matchDetails = $matchDetails . PHP_EOL . $setDetails . PHP_EOL;
+        return $matchDetails;
     }
 }
 ?>
